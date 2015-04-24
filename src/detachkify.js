d138834eb9b3f0a44beb14ba2766a85c80133b20
Adnan Hajdarevic <adnanh@gmail.com>
2015-04-24 16:07:27 +0200

    fix for windows path delimiters

--- src/detachkify.js
+++ src/detachkify.js
@@ -11,14 +11,20 @@ function transform(args, opts, cb) {
 
     opts.config.relativeTo = opts.config.relativeTo || process.cwd();
     var newRequire = path.relative(path.dirname(opts.file), path.join(opts.config.relativeTo, req));
+    newRequire = newRequire.replace(/\\/g, '/');
+
+    if (opts.config.verbose) {
+        console.log(args, newRequire);
+    }
+
     return cb(null, "require(\"" + newRequire + "\")");
 }
 
 // Transform options
-var opts = {
+var _opts = {
     evaluateArguments: true,
     jsFilesOnly: true
 };
 
 // Export transform
-module.exports = tools.makeRequireTransform("detachkify", opts, transform);
+module.exports = tools.makeRequireTransform("detachkify", _opts, transform);
