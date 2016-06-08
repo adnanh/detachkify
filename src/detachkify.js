6e1971e579fd2d737bee85004f9c019eb5155baa
Adnan Hajdarevic <adnanh@gmail.com>
2016-06-08 23:53:31 +0200

    resolve imports from different projects using detachkify using .detachkify-root file

--- src/detachkify.js
+++ src/detachkify.js
@@ -1,6 +1,7 @@
 var tools = require('browserify-transform-tools');
 var startsWith = require('starts-with');
 var path = require('path');
+var fs = require('fs');
 
 // Transform function
 function transform(args, opts, cb) {
@@ -10,12 +11,34 @@ function transform(args, opts, cb) {
     if (!startsWith(req, prefix)) {
       return cb();
     }
+
     if (prefix !== '/') {
       req = req.replace(opts.config.prefix, '');
     }
 
     opts.config.relativeTo = opts.config.relativeTo || process.cwd();
-    var newRequire = path.relative(path.dirname(opts.file), path.join(opts.config.relativeTo, req));
+
+    var newRequire;
+
+    if (startsWith(opts.file, opts.config.relativeTo)) {
+      newRequire = path.relative(path.dirname(opts.file), path.join(opts.config.relativeTo, req));
+    } else {
+      currentPath = path.parse(opts.file)
+      while(currentPath.dir !== currentPath.root) {
+        try {
+          fs.statSync(path.join(currentPath.dir, '.detachkify-root'));
+          newRequire = path.relative(path.dirname(opts.file), path.join(currentPath.dir, req));
+          break;
+        } catch(e) {
+          currentPath = path.parse(currentPath.dir);
+        }
+      }
+
+      if (newRequire === undefined) {
+        throw new Error('Looks like you are trying to include files from a project that uses detachkify with different relativeTo path. Please add a .detachkify-root file to the relativeTo path of that folder.');
+      }
+    }
+
     newRequire = newRequire.replace(/\\/g, '/');
 
     // in case the file is located in the current directory or a directory within the current directory, make sure to prepend './'
@@ -24,7 +47,7 @@ function transform(args, opts, cb) {
     }
 
     if (opts.config.verbose) {
-        console.log(args[0], newRequire);
+        console.log(args[0], ' -> ', newRequire);
     }
 
     return cb(null, 'require("' + newRequire + '")');
